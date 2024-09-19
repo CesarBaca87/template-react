05ae63b8951b9b23e872732df560cce54f4c7d6d
BacaCesar <cesarbaca.fotografia@gmail.com>
2024-09-19 13:16:26 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './styles/index.css';
+import './styles/normalize.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById('root');
+
+createRoot(container).render(<App />);
